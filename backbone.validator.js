5c57f20464ca628ab8abc72698ad135f0c7d4bce
satchmorun <rulfzid@gmail.com>
2012-01-16 14:06:49 -0600

    Tests and several validator messages.
    Many thanks be to jquery-validation for already doing a lot
    of the heavy lifting.

--- backbone.validator.js
+++ backbone.validator.js
@@ -80,7 +80,7 @@
       for (v in validators) {
         if (v === 'msg') continue;
         options = validators[v];
-        if (!Validator.validators[v](options, value)) return false;
+        if (!Validator.validators[v](value, options)) return false;
       }
 
       return true;
@@ -88,14 +88,173 @@
 
   });
 
+  Validator.regexen = {
+    // Contributed to jquery-validation by Scott Gonzalez:
+    // http://projects.scottsplayground.com/iri/
+    url: /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i,
+
+    // Same as `url`, but TLD is optional
+    url2: /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)*(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i,
+
+    // Contributed to jquery-validation by Scott Gonzalez:
+    // http://projects.scottsplayground.com/email_address_validation/
+    email: /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i,
+
+    // Same as `email`, with TLD optional
+    email2: /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)*(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i,
+
+    // From jquery-validation
+    number: /^-?(?:\d+|\d{1,3}(?:,\d{3})+)(?:\.\d+)?$/,
+
+    // From jquery-validation
+    phoneUS: /^(1-?)?(\([2-9]\d{2}\)|[2-9]\d{2})-?[2-9]\d{2}-?\d{4}$/
+
+  }
+
+
   Validator.validators = {
-    length: function(options, value) {
+    length: function(value, options) {
       var min = options.min || 0
         , max = options.max || Infinity
         , len = value.length;
 
+      if (min > max) {
+        throw new Error("Validator[length]: min can't be less then max");
+      }
+
       return len >= min && len <= max;
+    },
+
+    min: function(value, minValue) {
+      return value >= minValue;
+    },
+
+    max: function(value, maxValue) {
+      return value <= maxValue;
+    },
+
+    pattern: function(value, pattern) {
+      return pattern.test(value);
+    },
+
+    url: function(value) {
+      return Validator.regexen.url.test(value);
+    },
+
+    url2: function(value) {
+      return Validator.regexen.url2.test(value);
+    },
+
+    email: function(value) {
+      return Validator.regexen.email.test(value);
+    },
+
+    email2: function(value) {
+      return Validator.regexen.email2.test(value);
+    },
+
+    number: function(value) {
+      return Validator.regexen.number.test(value);
+    },
+
+    phoneUS: function(value) {
+      return Validator.regexen.phoneUS.test(value.replace(/\s+/g, ''));
+    },
+
+    /**
+     * Modified from jquery-validation.
+     *
+     * NOTICE: Modified version of
+     * Castle.Components.Validator.CreditCardValidator
+     *
+     * Redistributed under the the Apache License 2.0 at
+     * http://www.apache.org/licenses/LICENSE-2.0
+     *
+     * Valid Types: mastercard, visa, amex, dinersclub, enroute, discover, jcb,
+     * unknown, all (overrides all other settings)
+     */
+
+    creditcard: function(value, options) {
+
+      if (/[^0-9-]+/.test(value)) return false;
+
+      value = value.replace(/\D/g, "");
+
+      if (!luhnCheck(value)) return false;
+
+      var validTypes = 0x0000;
+
+      if (options.mastercard) validTypes |= 0x0001;
+      if (options.visa)       validTypes |= 0x0002;
+      if (options.amex)       validTypes |= 0x0004;
+      if (options.dinersclub) validTypes |= 0x0008;
+      if (options.enroute)    validTypes |= 0x0010;
+      if (options.discover)   validTypes |= 0x0020;
+      if (options.jcb)        validTypes |= 0x0040;
+      if (options.unknown)    validTypes |= 0x0080;
+      if (options.all)        validTypes = 0x0001 | 0x0002 | 0x0004 | 0x0008 |
+                                           0x0010 | 0x0020 | 0x0040 | 0x0080;
+
+      // MasterCard
+      if (validTypes & 0x0001 && /^(51|52|53|54|55)/.test(value)) {
+        return value.length == 16;
+      }
+      // VISA
+      if (validTypes & 0x0002 && /^(4)/.test(value)) {
+        return value.length == 16;
+      }
+      // AMEX
+      if (validTypes & 0x0004 && /^(34|37)/.test(value)) {
+        return value.length == 15;
+      }
+      // DinersClub
+      if (validTypes & 0x0008 &&
+          /^(300|301|302|303|304|305|36|38)/.test(value)) {
+        return value.length == 14;
+      }
+      // Enroute
+      if (validTypes & 0x0010 && /^(2014|2149)/.test(value)) {
+        return value.length == 15;
+      }
+      // Discover
+      if (validTypes & 0x0020 && /^(6011)/.test(value)) {
+        return value.length == 16;
+      }
+      // JCB
+      if (validTypes & 0x0040 && /^(3)/.test(value)) {
+        return value.length == 16;
+      }
+      // JCB
+      if (validTypes & 0x0040 && /^(2131|1800)/.test(value)) {
+        return value.length == 15;
+      }
+      // Unknown
+      if (validTypes & 0x0080) {
+        return true;
+      }
+      return false;
     }
+
   };
+
+  /**
+   * Run a luhn digit check on a string of digits. Used by the `creditcard`
+   * validator.
+   */
+
+  function luhnCheck(value) {
+    var i, digit, even = false, check = 0;
+
+    for (i = value.length - 1; i >= 0; i--) {
+      digit = parseInt(value.charAt(i));
+      if (even) {
+        if ((digit *= 2) > 9) digit -= 9;
+      }
+      check += digit;
+      even = !even;
+    }
+
+    return (check % 10) === 0;
+  }
 
 }(_, Backbone));
---
+++ test/test.validator.methods.js
@@ -0,0 +1,210 @@
+/**
+ * Tests adapted from the excellent jquery-validation library:
+ * https://github.com/jzaefferer/jquery-validation
+*/
+
+$(function() {
+
+  function testValidator(name, options) {
+    var method = Backbone.Validator.validators[name];
+    return function(value) {
+      return method(value, options);
+    };
+  }
+
+  module("Validator Methods");
+
+  test("url", function() {
+    var method = testValidator("url");
+    ok( method( "http://bassistance.de/jquery/plugin.php?bla=blu" ), "Valid url" );
+    ok( method( "https://bassistance.de/jquery/plugin.php?bla=blu" ), "Valid url" );
+    ok( method( "ftp://bassistance.de/jquery/plugin.php?bla=blu" ), "Valid url" );
+    ok( method( "http://www.føtex.dk/" ), "Valid url, danish unicode characters" );
+    ok( method( "http://bösendorfer.de/" ), "Valid url, german unicode characters" );
+    ok( method( "http://192.168.8.5" ), "Valid IP Address" )
+    ok(!method( "http://192.168.8." ), "Invalid IP Address" )
+    ok(!method( "http://bassistance" ), "Invalid url" ); // valid
+    ok(!method( "http://bassistance." ), "Invalid url" ); // valid
+    ok(!method( "http://bassistance,de" ), "Invalid url" );
+    ok(!method( "http://bassistance;de" ), "Invalid url" );
+    ok(!method( "http://.bassistancede" ), "Invalid url" );
+    ok(!method( "bassistance.de" ), "Invalid url" );
+  });
+
+  test("url2 (tld optional)", function() {
+    var method = testValidator("url2");
+    ok( method( "http://bassistance.de/jquery/plugin.php?bla=blu" ), "Valid url" );
+    ok( method( "https://bassistance.de/jquery/plugin.php?bla=blu" ), "Valid url" );
+    ok( method( "ftp://bassistance.de/jquery/plugin.php?bla=blu" ), "Valid url" );
+    ok( method( "http://www.føtex.dk/" ), "Valid url, danish unicode characters" );
+    ok( method( "http://bösendorfer.de/" ), "Valid url, german unicode characters" );
+    ok( method( "http://192.168.8.5" ), "Valid IP Address" )
+    ok(!method( "http://192.168.8." ), "Invalid IP Address" )
+    ok( method( "http://bassistance" ), "Invalid url" );
+    ok( method( "http://bassistance." ), "Invalid url" );
+    ok(!method( "http://bassistance,de" ), "Invalid url" );
+    ok(!method( "http://bassistance;de" ), "Invalid url" );
+    ok(!method( "http://.bassistancede" ), "Invalid url" );
+    ok(!method( "bassistance.de" ), "Invalid url" );
+  });
+
+  test("email", function() {
+    var method = testValidator("email");
+    ok( method( "name@domain.tld" ), "Valid email" );
+    ok( method( "name@domain.tl" ), "Valid email" );
+    ok( method( "bart+bart@tokbox.com" ), "Valid email" );
+    ok( method( "bart+bart@tokbox.travel" ), "Valid email" );
+    ok( method( "n@d.tld" ), "Valid email" );
+    ok( method( "ole@føtex.dk"), "Valid email" );
+    ok( method( "jörn@bassistance.de"), "Valid email" );
+    ok( method( "bla.blu@g.mail.com"), "Valid email" );
+    ok( method( "\"Scott Gonzalez\"@example.com" ), "Valid email" );
+    ok( method( "\"Scott González\"@example.com" ), "Valid email" );
+    ok( method( "\"name.\"@domain.tld" ), "Valid email" ); // valid without top label
+    ok( method( "\"name,\"@domain.tld" ), "Valid email" ); // valid without top label
+    ok( method( "\"name;\"@domain.tld" ), "Valid email" ); // valid without top label
+    ok(!method( "name" ), "Invalid email" );
+    ok(!method( "name@" ), "Invalid email" );
+    ok(!method( "name@domain" ), "Invalid email" );
+    ok(!method( "name.@domain.tld" ), "Invalid email" );
+    ok(!method( "name,@domain.tld" ), "Invalid email" );
+    ok(!method( "name;@domain.tld" ), "Invalid email" );
+    ok(!method( "name;@domain.tld." ), "Invalid email" );
+  });
+
+  test("email2 (tld optional)", function() {
+    var method = testValidator("email2");
+    ok( method( "name@domain.tld" ), "Valid email" );
+    ok( method( "name@domain.tl" ), "Valid email" );
+    ok( method( "bart+bart@tokbox.com" ), "Valid email" );
+    ok( method( "bart+bart@tokbox.travel" ), "Valid email" );
+    ok( method( "n@d.tld" ), "Valid email" );
+    ok( method( "ole@føtex.dk"), "Valid email" );
+    ok( method( "jörn@bassistance.de"), "Valid email" );
+    ok( method( "bla.blu@g.mail.com"), "Valid email" );
+    ok( method( "\"Scott Gonzalez\"@example.com" ), "Valid email" );
+    ok( method( "\"Scott González\"@example.com" ), "Valid email" );
+    ok( method( "\"name.\"@domain.tld" ), "Valid email" ); // valid without top label
+    ok( method( "\"name,\"@domain.tld" ), "Valid email" ); // valid without top label
+    ok( method( "\"name;\"@domain.tld" ), "Valid email" ); // valid without top label
+    ok(!method( "name" ), "Invalid email" );
+    ok(!method( "name@" ), "Invalid email" );
+    ok( method( "name@domain" ), "Invalid email" );
+    ok(!method( "name.@domain.tld" ), "Invalid email" );
+    ok(!method( "name,@domain.tld" ), "Invalid email" );
+    ok(!method( "name;@domain.tld" ), "Invalid email" );
+  });
+
+  test("number", function() {
+    var method = testValidator("number");
+    ok( method( "123" ), "Valid number" );
+    ok( method( "-123" ), "Valid number" );
+    ok( method( "123,000" ), "Valid number" );
+    ok( method( "-123,000" ), "Valid number" );
+    ok( method( "123,000.00" ), "Valid number" );
+    ok( method( "-123,000.00" ), "Valid number" );
+    ok(!method( "123.000,00" ), "Invalid number" );
+    ok(!method( "123.0.0,0" ), "Invalid number" );
+    ok(!method( "x123" ), "Invalid number" );
+    ok(!method( "100.100,0,0" ), "Invalid number" );
+
+    ok( method( "123" ), "Valid decimal" );
+    ok( method( "123000" ), "Valid decimal" );
+    ok( method( "123000.12" ), "Valid decimal" );
+    ok( method( "-123000.12" ), "Valid decimal" );
+    ok( method( "123.000" ), "Valid decimal" );
+    ok( method( "123,000.00" ), "Valid decimal" );
+    ok( method( "-123,000.00" ), "Valid decimal" );
+    ok(!method( "1230,000.00" ), "Invalid decimal" );
+    ok(!method( "123.0.0,0" ), "Invalid decimal" );
+    ok(!method( "x123" ), "Invalid decimal" );
+    ok(!method( "100.100,0,0" ), "Invalid decimal" );
+  });
+
+  test('length', function() {
+    var min = testValidator('length', {min: 5});
+    ok( min('123456'), 'Longer than min');
+    ok( min('12345'), 'At min length');
+    ok(!min('1234'), 'Shorter than min');
+
+    var max = testValidator('length', {max: 5});
+    ok(!max('123456'), 'Longer than max');
+    ok( max('12345'), 'At max length');
+    ok( max('1234'), 'Shorter than max');
+
+    var minmax = testValidator('length', {min: 5, max: 10});
+    ok(!minmax('1234'), 'Shorter than min');
+    ok( minmax('12345'), 'At min');
+    ok( minmax('1234567890'), 'At max');
+    ok(!minmax('12345678901'), 'Longer than max');
+
+    var pathological = testValidator('length', {min:4, max: 2});
+    raises(function() { pathological('1234'); });
+  });
+
+
+  test("min", function() {
+    var min = testValidator('min', 5)
+    ok( min('5'), 'Valid string at min');
+    ok( min('6'), 'Valid string > min');
+    ok(!min('3'), 'Valid string < min');
+    ok(!min('asdf'), 'Invalid string');
+  });
+
+  test("max", function() {
+    var max = testValidator('max', 5)
+    ok( max('5'), 'Valid string at max');
+    ok(!max('6'), 'Valid string > max');
+    ok( max('3'), 'Valid string < max');
+    ok(!max('asdf'), 'Invalid string');
+  });
+
+  test("phone (us)", function() {
+    var method = testValidator("phoneUS");
+    ok( method( "1(212)-999-2345" ), "Valid us phone number" );
+    ok( method( "212 999 2344" ), "Valid us phone number" );
+    ok( method( "212-999-0983" ), "Valid us phone number" );
+    ok(!method( "111-123-5434" ), "Invalid us phone number" );
+    ok(!method( "212 123 4567" ), "Invalid us phone number" );
+  });
+
+  test("pattern", function() {
+    var pat = testValidator("pattern", /^AR\d{4}$/);
+    ok( pat( "AR1004" ), "Correct format for the given RegExp" );
+    ok( !pat( "BR1004" ), "Invalid format for the given RegExp" );
+  });
+
+  test('creditcardtypes, all', function() {
+    var cc = testValidator('creditcard', {all: true});
+
+    equals(cc("4111-1111-1111-1111"), true, 'Valid VISA');
+    equals(cc("5111-1111-1111-1118"), true, 'Valid MasterCard');
+    equals(cc("6111-1111-1111-1116"), true, 'Valid Discover');
+    equals(cc("3400-0000-0000-009"), true, 'Valid AMEX');
+
+    equals(cc("4111-1111-1111-1110"), false, 'Invlaid VISA');
+    equals(cc("5432-1111-1111-1111"), false, 'Invalid MasterCard');
+    equals(cc("6611-6611-6611-6611"), false, 'Invalid Discover');
+    equals(cc("3777-7777-7777-7777"), false, 'Invalid AMEX');
+
+  });
+
+  test('creditcardtypes, visa', function() {
+    var visa = testValidator('creditcard', {visa: true});
+
+    equals(visa("4111-1111-1111-1111"), true, 'VISA');
+    equals(visa("5111-1111-1111-1118"), false, 'MasterCard');
+    equals(visa("6111-1111-1111-1116"), false, 'Discover');
+    equals(visa("3400-0000-0000-009"), false, 'AMEX');
+  });
+
+  test('creditcardtypes, mastercard', function() {
+    var mastercard = testValidator('creditcard', {mastercard: true});
+
+    equals(mastercard("5111-1111-1111-1118"), true, 'MasterCard');
+    equals(mastercard("6111-1111-1111-1116"), false, 'Discover');
+    equals(mastercard("3400-0000-0000-009"), false, 'AMEX');
+    equals(mastercard("4111-1111-1111-1111"), false, 'VISA');
+  });
+
+});
